f85e8a972dc68608f3984e9c05b1a22b678a83fa
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2025-05-02 00:31:51 +0600

    shadcn installed and mode toggoler added using shadcn

--- src/app/(generalLayout)/page.tsx
+++ src/app/(generalLayout)/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
+import { ModeToggle } from "@/components/ui/mode-toggler";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Home page</h1>
+      <DropdownMenu>dropdown</DropdownMenu>
+      <ModeToggle></ModeToggle>
+      <Button>Secondary</Button>
     </div>
   );
 }
